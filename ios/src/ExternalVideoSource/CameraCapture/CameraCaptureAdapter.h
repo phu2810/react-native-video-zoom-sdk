ab45e6813a61a0d6a9c2be71e9250bd12652d509
Phu Nguyen <nguyenphu2810@gmail.com>
2021-05-17 17:18:16 +0700

    delete some class

--- ios/src/ExternalVideoSource/CameraCapture/CameraCaptureAdapter.h
+++
@@ -1,9 +0,0 @@
-
-#import <ZoomInstantSDK/ZoomInstantSDK.h>
-#import <AVFoundation/AVFoundation.h>
-#import <AssetsLibrary/AssetsLibrary.h>
-
-@interface CameraCaptureAdapter : NSObject <ZoomInstantSDKVideoSource, AVCaptureVideoDataOutputSampleBufferDelegate>
-
-@end
-
--- ios/src/ExternalVideoSource/SendPicture/SendPictureAdapter.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface SendPictureAdapter : NSObject <ZoomInstantSDKVideoSource>
-
-@end
-
-NS_ASSUME_NONNULL_END
