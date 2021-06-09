53397c59b8de42a13862c5885ac1a0fa9a9de24c
votheanh39 <anhtv1994@gmail.com>
2021-06-09 11:39:16 +0700

    update android share screen

--- android/src/main/java/com/reactnativevideozoomsdk/VideoZoomSdkPackage.java
+++ android/src/main/java/com/reactnativevideozoomsdk/VideoZoomSdkPackage.java
@@ -7,19 +7,21 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class VideoZoomSdkPackage implements ReactPackage {
-    @NonNull
-    @Override
-    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
-      return Collections.singletonList(new VideoZoomSdkModule(reactContext));
-    }
 
-    @NonNull
-    @Override
-    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
-        return Collections.singletonList(new ZoomViewManager());
-    }
+  @NonNull
+  @Override
+  public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+    return Collections.singletonList(new VideoZoomSdkModule(reactContext));
+  }
+
+  @NonNull
+  @Override
+  public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+    return Arrays.asList(new ZoomViewManager(), new ZoomShareViewManager());
+  }
 }
